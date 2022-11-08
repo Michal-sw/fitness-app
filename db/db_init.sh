75a7fcc6795c85856f3ff7202ce85ea0abfff92b
= <florian.dobkowski@gmail.com>
2022-11-08 19:51:04 +0100

    changed postgres init_db script

---
+++ db/db_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+psql -U $POSTGRES_USER <<-EOSQL
+    \c $POSTGRES_DB;
+
+    BEGIN;
+        CREATE TABLE IF NOT EXISTS "user" (
+            id serial PRIMARY KEY,
+            username VARCHAR ( 50 ) UNIQUE NOT NULL,
+            password VARCHAR ( 50 ) NOT NULL,
+            email VARCHAR ( 255 ) UNIQUE NOT NULL,
+            created_on TIMESTAMP NOT NULL
+        );
+    COMMIT;
+EOSQL
