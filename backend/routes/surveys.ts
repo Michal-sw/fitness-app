338313ae68208ab3b2fbd2addf0a52e99a8bcaac
fdobkowski <florian.dobkowski@gmail.com>
2023-01-24 19:22:57 +0100

    fixed minor endpoint bugs

--- backend/routes/surveys.ts
+++ backend/routes/surveys.ts
@@ -1,15 +1,11 @@
 import express, { Request, Response, Router } from 'express';
-import { JwtPayload, VerifyErrors } from 'jsonwebtoken';
 import { authorizeMiddleware } from '../middlewares/middlewares';
-import utils from '../utils/utils';
-import { addUser, getUsers, getUserIfCredentialValid, getUserByLogin } from '../services/userService';
-import { IUser } from '../config/models/User';
 import { addSurvey, finishSurvey, getSurveys } from '../services/surveyService';
 
 const router: Router = express.Router({mergeParams: true});
 
-router.get('/', authorizeMiddleware, async (req: Request, res: Response) => {
-  const response: any = await getSurveys(req.body.id);
+router.get('/:id', authorizeMiddleware, async (req: Request, res: Response) => {
+  const response: any = await getSurveys({ id: req.params.id });
   res.status(response.statusCode).json(response);
 });
 
@@ -19,8 +15,8 @@ router.post('/', authorizeMiddleware, async (req: Request, res: Response) => {
     res.status(response.statusCode).json(response);
 });
 
-router.patch('/:id', authorizeMiddleware, async (req: Request, res: Response) => {
-    const response: any = await finishSurvey({ id: req.params.id, ...req.body });
+router.patch('/', authorizeMiddleware, async (req: Request, res: Response) => {
+    const response: any = await finishSurvey({ id: req.body.id, ...req.body });
 
     res.status(response.statusCode).json(response);
 });
--- backend/services/surveyService.ts
+++ backend/services/surveyService.ts
@@ -1,31 +1,43 @@
 import { MongooseError } from "mongoose";
 import Survey, { ISurvey } from "../config/models/Surveys";
-import User from "../config/models/User";
 
 
 const getCorrectObject = (result: any) => ({ result, statusCode: 200 })
 const getErrorObject = (statusCode: number, message?: string) => ({ statusCode, result: message })
 
-export const getSurveys = async ({ id }: { id: number }) => {
+export const getSurveys = async ({ id }: { id: string }) => {
     const result = await Survey
-        .find({ user: User.findById(id)})
+        .find({ user: id })
         .then((surveys: [ISurvey] | any) => getCorrectObject(surveys))
         .catch((err: MongooseError) => getErrorObject(500, err.message));
 
     return result;
 }
 
-export const addSurvey = async ({ id }: { id: number }) => {
+export const addSurvey = async (id: string) => {
+
+    // Check if the user already got a survey today
+
+    const surveys = await getSurveys({ id: id });
+    const now = new Date();
+    const todaySurveys = surveys.result.filter((x: any) => {
+        const date = new Date(x.date)
+        return (date.getFullYear() === now.getFullYear() &&
+                date.getMonth() === now.getMonth() &&
+                date.getDate() == now.getDate())
+    })
+
+    if (todaySurveys.length > 0) return { statusCode: 200, completed: true}
 
     const result = await Survey.create({
-        user: User.findById(id),
+        user: id,
         date: new Date(),
         waterScore: 0,
         sleepScore: 0,
         trainingScore: 0,
         hasBeenChecked: false
     })
-    .then((survey: ISurvey) => getCorrectObject(survey))
+    .then((survey: ISurvey) => getCorrectObject({ ...survey, amount: surveys.result.length + 1 }))
     .catch((err: MongooseError) => getErrorObject(400, err.message));
 
     return result;
@@ -33,7 +45,8 @@ export const addSurvey = async ({ id }: { id: number }) => {
 
 export const finishSurvey = async ({ id, ...body }: { id: string}) => {
     
-    const survey = await Survey.findOneAndReplace({id}, { ...body, date: new Date(), hasBeenChecked: true });
-    
-    return getCorrectObject(survey);
+    const survey = await Survey.findByIdAndUpdate(id, { ...body, date: new Date(), hasBeenChecked: true })
+        .then((survey: any) => getCorrectObject(survey))
+        .catch((err: MongooseError) => getErrorObject(400, err.message));
+    return survey;
 }
