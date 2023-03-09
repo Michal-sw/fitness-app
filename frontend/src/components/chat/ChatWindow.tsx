3bd60d9a0cc9f179c8adc3f3aa901dca17a151e1
Michał Świerczewski <michal.swierczewski13@wp.pl>
2023-03-09 14:11:11 +0100

    Code split chat component

--- frontend/src/components/chat/ChatWindow.tsx
+++ frontend/src/components/chat/ChatWindow.tsx
@@ -2,11 +2,11 @@ import { useChat } from "../../hooks/useChat";
 import useWebSocket from "../../core/providers/WebSocketContext";
 import { useEffect, useRef, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import ChatInput from "./ChatInput";
 
 const ChatWindow = ({ activityId }: { activityId: string }) => {
-    const { sendMessage, chatHistory } = useChat(activityId);
+    const { chatHistory, sendMessage } = useChat(activityId);
     const { leaveChatRoom } = useWebSocket();
-    const [messageInput, setMessageInput] = useState('');
     const lastMessageRef = useRef<HTMLDivElement|null>(null);
 
     useEffect(() => {
@@ -14,26 +14,8 @@ const ChatWindow = ({ activityId }: { activityId: string }) => {
         lastMessageRef.current.scrollIntoView({});
         lastMessageRef.current.style.height = '20px';
     }, [chatHistory.length])
-  
-    const handleMessageChange = (event: any) => {
-        setMessageInput(event.target.value)
-        event.target.style.height = "30px";
-        event.target.style.height = `${event.target.scrollHeight}px`;  
-     };
-  
-     const publishMessage = () => {
-        const trimmedMessage = messageInput.trim();
-        if (!messageInput || trimmedMessage.length === 0) return;
-        sendMessage(trimmedMessage);
-        setMessageInput('');
-     } 
-  
-    const handleKeyboardStroke = (event: any) => {
-      if (event.keyCode === 13 && !event.shiftKey) {
-        publishMessage()
-      }
-    }
-  
+    
+    console.log("render");
     return (
         <div className="chat-container">
             <button onClick={() => leaveChatRoom(activityId)}>&#10006;</button>
@@ -47,15 +29,7 @@ const ChatWindow = ({ activityId }: { activityId: string }) => {
                 )}
                 <div ref={lastMessageRef}/>
             </div>
-            <div className="message-input-container">
-                <textarea 
-                    value={messageInput}
-                    placeholder='message'
-                    onChange={handleMessageChange}
-                    onKeyUp={handleKeyboardStroke}
-                />
-                <button onClick={publishMessage}>Send</button>
-            </div>
+            <ChatInput sendMessage={sendMessage}/>
 
         </div>
     );
