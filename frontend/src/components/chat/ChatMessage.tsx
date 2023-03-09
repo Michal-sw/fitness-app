953537c8a0c0d67271066b97afea0d902e2f5851
Michał Świerczewski <michal.swierczewski13@wp.pl>
2023-03-09 13:19:36 +0100

    add style separation for user messages

--- frontend/src/components/chat/ChatMessage.tsx
+++ frontend/src/components/chat/ChatMessage.tsx
@@ -1,11 +1,16 @@
+import useAuth from "../../core/providers/AuthContext";
 import { ChatMessageDT } from "../../core/types/ChatMessageDT";
 import "../../styles/chat/ChatMessage.scss";
 
 const ChatMessage = ({message}: { message: ChatMessageDT }) => {
-  
+    const { user } = useAuth();
+    const isAuthor = message.author === user.login;
+
     return (
-        <div className="chat-message">
-            <span className="chat-message-author">{message.author}:</span>
+        <div className={`chat-message ${isAuthor ? 'local-user' : null}`}>
+            {!isAuthor && 
+                <span className="chat-message-author">{message.author}:</span>
+            }
             <span className="chat-message-content">{message.text}</span>
         </div>
     );
